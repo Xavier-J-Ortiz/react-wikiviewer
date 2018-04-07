da5815189482c6776c369b1f530535be75e4e638
xortiz <xavier.ortiz.ch@gmail.com>
2018-04-07 18:36:05 -0400

    stuck in trying to fetch jsonp

--- src/components/WikiViewer/WikiViewer.js
+++ src/components/WikiViewer/WikiViewer.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+//import jsonp from 'jsonp';
+//import axios  from 'axios'
 //import WikiViewer from './logo.svg';
 //import './App.css';
 //import './WikiViewer.js';
@@ -7,12 +9,21 @@ class WikiViewer extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { queryAnswer : null};
 
     this.getSearchTerm = (e) => {
       if (e.key === 'Enter') {
         e.preventDefault();
-        let searchURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + encodeURI(e.target.value) + "&limit=10&namespace=0&format=json";
+
+        let searchURL = "https://en.wikipedia.org/w/api.php?origin=*&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + encodeURI(e.target.value) + "&format=json";
         console.log(searchURL);
+        fetch(searchURL, { mode: 'no-cors' })
+          .then( results => {
+            return results;
+          })
+          .then( data => {
+            this.setState({ queryAnswer: data.query });
+          } );
       }
     };
 
@@ -29,6 +40,7 @@ class WikiViewer extends Component {
         </form>
         <p>
           write in text box for a search<br/>
+          { this.state.queryAnswer ? this.state.queryAnswer : ''  }
         </p>
       </div>
     );
