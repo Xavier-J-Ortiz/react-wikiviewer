01249a09c17d77a07d857bfc5f1c8ca1848cfc65
xortiz <xavier.ortiz.ch@gmail.com>
2018-04-07 09:26:25 -0400

    first commit for wikiviewer. Adding a skeleton of components and how I'd like to lay this out

---
+++ src/components/WikiViewer/WikiViewer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import WikiViewer from './logo.svg';
+//import './App.css';
+//import './WikiViewer.js';
+
+class WikiViewer extends Component {
+  render() {
+    return (
+        <p className="Main">
+          place wikiviewer here
+        </p>
+    );
+  }
+}
+
+export default WikiViewer;
